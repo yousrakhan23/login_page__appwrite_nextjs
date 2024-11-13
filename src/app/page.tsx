69cd183c692485159b00c7cb8c416284cc6c8705
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-11-13 05:44:45 +0500

    update all files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ function LoginPage() {
         <button 
         type="button" 
         onClick={() => login(email, password)}
-        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+        className="w-full px-4 py-2 text-blue-600 border border-blue-600 rounded-lg hover:bg-blue-600 hover:text-white transition-colors">
         
         Login
         </button>
